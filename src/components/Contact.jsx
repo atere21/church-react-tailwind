111203ebd6743a7011fddbd3ef2172500515e8f8
Oluwatosin Atere <tosin.atere@sanlam.com.ng>
2024-02-14 15:11:06 +0100

    church

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   return (
-    <div name='contact' className='max-w-[1140px] m-auto w-full p-4 py-16'>
-      <h2 className='text-center text-gray-700 text-4xl'>Send us a message</h2>
-      <p className='text-center py-2 text-blue-700 font-bold'>We're standing by!</p>
-      <div className='grid md:grid-cols-2'>
+    <div name="contact" className="max-w-[1140px] m-auto w-full p-4 py-16">
+      <h2 className="text-center text-gray-700 text-4xl">Send us a message</h2>
+      <p className="text-center py-2 text-blue-700 font-bold">
+        We're standing by!
+      </p>
+      <div className="grid md:grid-cols-2">
         <img
-          src='https://media.istockphoto.com/id/847575018/photo/group-of-people-holding-hands-praying-worship-believe.jpg?s=612x612&w=0&k=20&c=hbklAfDraUwmVBE6X0DAOnc9uu_QAIIe4z3drqyM1LA='
-          alt='/'
-          className='w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]'
+          src="https://media.istockphoto.com/id/847575018/photo/group-of-people-holding-hands-praying-worship-believe.jpg?s=612x612&w=0&k=20&c=hbklAfDraUwmVBE6X0DAOnc9uu_QAIIe4z3drqyM1LA="
+          alt="/"
+          className="w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]"
         />
-      
-        <form action="https://getform.io/f/184c2acc-90fb-4ead-9cdc-ed9cafd676a5" method="POST">
-      
-          <div className='grid grid-cols-2'>
-            <input className='border m-2 p-2' name='first' type='text' placeholder='First' />
-            <input className='border m-2 p-2' name='last' type='text' placeholder='Last' />
-            <input className='border m-2 p-2' name='email' type='email' placeholder='Email' />
-            <input className='border m-2 p-2' name='phone' type='tel' placeholder='Phone' />
-            <input className='border col-span-2 p-2 m-2' name='text' type='text' placeholder='Address'/>
-            <textarea className='border col-span-2 m-2 p-2 resize-none' name='textarea' cols='30' rows='10'></textarea>
-            <button className='col-span-2 m-2'>Submit</button>
+
+        <form action="https://getform.io/f/DbDGjyal" method="POST">
+          <div className="grid grid-cols-2">
+            <input
+              className="border m-2 p-2"
+              name="first"
+              type="text"
+              placeholder="First"
+            />
+            <input
+              className="border m-2 p-2"
+              name="last"
+              type="text"
+              placeholder="Last"
+            />
+            <input
+              className="border m-2 p-2"
+              name="email"
+              type="email"
+              placeholder="Email"
+            />
+            <input
+              className="border m-2 p-2"
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+            />
+            <input
+              className="border col-span-2 p-2 m-2"
+              name="text"
+              type="text"
+              placeholder="Address"
+            />
+            <textarea
+              className="border col-span-2 m-2 p-2 resize-none"
+              name="textarea"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className="col-span-2 m-2">Submit</button>
           </div>
         </form>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
       <div className="max-w-6xl mx-auto px-4 py-8 border-t-2 border-gray-600">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:flex gap-8">
           <div className="md:col-span-2">
-            <h6 className="text-2xl font-bold uppercase mb-4 mx-4">Information</h6>
+            <h6 className="text-2xl font-bold uppercase mb-4 mx-4">
+              Information
+            </h6>
             <ul className="text-gray-400">
-              <li className="py-2">
-                Email: Twprayerministry@gmail.com
-              </li>
+              <li className="py-2">Email: Twprayerministry@gmail.com</li>
               <li className="py-2">Sunday Service: 11:30 AM - 1:00 PM</li>
               <li className="py-2">Phone: +447496021086</li>
             </ul>
@@ -20,7 +20,8 @@ const Footer = () => {
             <h6 className="text-2xl font-bold uppercase mb-4 mx-4">Location</h6>
             <ul className="text-gray-400">
               <li className="py-2 cursor-pointer">
-                Address: 115 Cricklade Road, Swindon SN2 1AB United Kingdom Office
+                Address: 115 Cricklade Road, Swindon SN2 1AB United Kingdom
+                Office
               </li>
             </ul>
           </div>
@@ -29,7 +30,8 @@ const Footer = () => {
               Subscribe to our newsletter
             </h6>
             <p className="text-gray-400 py-2">
-              Get Sermons, listen to our podcast, we'll have it sent to your inbox weekly.
+              Get Sermons, listen to our podcast, we'll have it sent to your
+              inbox weekly.
             </p>
             <form className="flex flex-col sm:flex-row items-center">
               <input
@@ -45,17 +47,26 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 py-4 flex flex-col md:flex text-center text-gray-700">
+      <div className="max-w-6xl mx-auto px-4 py-4 flex flex-col md:flex text-center text-gray-300">
         <p className="py-4">Designed by Webmuse</p>
         <p className="py-4">&copy; 2023 The Word Prayer Ministries</p>
         <div className="flex justify-center  w-full pt-4 text-2xl cursor-pointer">
-          <a href="https://youtube.com/@TheWordPrayerMinistry?si=5Z5yIuqsFixIGVQg" className="mx-2 hover:text-yellow-600">
+          <a
+            href="https://youtube.com/@TheWordPrayerMinistry?si=5Z5yIuqsFixIGVQg"
+            className="mx-2 hover:text-yellow-600"
+          >
             <BsYoutube />
           </a>
-          <a href=" https://instagram.com/twprayerministry?igshid=MzRlODBiNWFlZA==" className="mx-2 hover:text-yellow-600">
+          <a
+            href=" https://instagram.com/twprayerministry?igshid=MzRlODBiNWFlZA=="
+            className="mx-2 hover:text-yellow-600"
+          >
             <BsInstagram />
           </a>
-          <a href="https://www.facebook.com/profile.php?id=100066928100987" className="mx-2 hover:text-yellow-600">
+          <a
+            href="https://www.facebook.com/profile.php?id=100066928100987"
+            className="mx-2 hover:text-yellow-600"
+          >
             <BsFacebook />
           </a>
         </div>
